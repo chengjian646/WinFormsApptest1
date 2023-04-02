2560c0db8fd5a4476ddf67f3a6b6aa9d17cf2e52
chengjian <2280896448@qq.com>
2023-04-02 20:48:45 +0800

    添加项目文件。

---
+++ server.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace WinFormsApptest1
+{
+    partial class server
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.startserver = new System.Windows.Forms.Button();
+            this.closeserver = new System.Windows.Forms.Button();
+            this.IPAddr = new System.Windows.Forms.TextBox();
+            this.Port = new System.Windows.Forms.TextBox();
+            this.Serstatus = new System.Windows.Forms.TextBox();
+            this.ShowDetail = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SendSize = new System.Windows.Forms.TextBox();
+            this.WindLoca1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "服务器 IP地址：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(349, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "端口号：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "服务器状态：";
+            // 
+            // startserver
+            // 
+            this.startserver.Location = new System.Drawing.Point(299, 79);
+            this.startserver.Name = "startserver";
+            this.startserver.Size = new System.Drawing.Size(93, 29);
+            this.startserver.TabIndex = 3;
+            this.startserver.Text = "启动服务器";
+            this.startserver.UseVisualStyleBackColor = true;
+            this.startserver.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // closeserver
+            // 
+            this.closeserver.Location = new System.Drawing.Point(411, 79);
+            this.closeserver.Name = "closeserver";
+            this.closeserver.Size = new System.Drawing.Size(93, 29);
+            this.closeserver.TabIndex = 4;
+            this.closeserver.Text = "关闭服务器";
+            this.closeserver.UseVisualStyleBackColor = true;
+            this.closeserver.Click += new System.EventHandler(this.closeserver_Click);
+            // 
+            // IPAddr
+            // 
+            this.IPAddr.Location = new System.Drawing.Point(173, 41);
+            this.IPAddr.Name = "IPAddr";
+            this.IPAddr.Size = new System.Drawing.Size(124, 27);
+            this.IPAddr.TabIndex = 5;
+            this.IPAddr.Text = "127.0.0.1";
+            // 
+            // Port
+            // 
+            this.Port.Location = new System.Drawing.Point(424, 41);
+            this.Port.Name = "Port";
+            this.Port.Size = new System.Drawing.Size(124, 27);
+            this.Port.TabIndex = 6;
+            this.Port.Text = "10086";
+            // 
+            // Serstatus
+            // 
+            this.Serstatus.Location = new System.Drawing.Point(156, 79);
+            this.Serstatus.Name = "Serstatus";
+            this.Serstatus.Size = new System.Drawing.Size(124, 27);
+            this.Serstatus.TabIndex = 7;
+            this.Serstatus.Text = "未启动";
+            // 
+            // ShowDetail
+            // 
+            this.ShowDetail.Location = new System.Drawing.Point(51, 206);
+            this.ShowDetail.Multiline = true;
+            this.ShowDetail.Name = "ShowDetail";
+            this.ShowDetail.Size = new System.Drawing.Size(497, 386);
+            this.ShowDetail.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(51, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "发送窗口大小：";
+            // 
+            // SendSize
+            // 
+            this.SendSize.Location = new System.Drawing.Point(156, 121);
+            this.SendSize.Name = "SendSize";
+            this.SendSize.Size = new System.Drawing.Size(84, 27);
+            this.SendSize.TabIndex = 10;
+            this.SendSize.Text = "5";
+            // 
+            // WindLoca1
+            // 
+            this.WindLoca1.Location = new System.Drawing.Point(156, 166);
+            this.WindLoca1.Name = "WindLoca1";
+            this.WindLoca1.Size = new System.Drawing.Size(420, 27);
+            this.WindLoca1.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(51, 169);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "接收窗口：";
+            // 
+            // server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 604);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.WindLoca1);
+            this.Controls.Add(this.SendSize);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ShowDetail);
+            this.Controls.Add(this.Serstatus);
+            this.Controls.Add(this.Port);
+            this.Controls.Add(this.IPAddr);
+            this.Controls.Add(this.closeserver);
+            this.Controls.Add(this.startserver);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Location = new System.Drawing.Point(1100, 200);
+            this.Name = "server";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "接收端";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button startserver;
+        private System.Windows.Forms.Button closeserver;
+        private System.Windows.Forms.TextBox IPAddr;
+        private System.Windows.Forms.TextBox Port;
+        private System.Windows.Forms.TextBox Serstatus;
+        private System.Windows.Forms.TextBox ShowDetail;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox SendSize;
+        private System.Windows.Forms.TextBox WindLoca1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ server.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WinFormsApptest1
+{
+    
+    public partial class server : Form
+    {
+        
+        private Thread readThread;
+        private Thread timelimitThread;
+        private Thread ShowThread;
+        private Socket socketatServer;
+        private TcpListener MyListener;
+        private bool ConnectOnline;
+        private int P1 = 0;
+        private int P3 = 4;
+        private int accumulator=0;//报文段累加器
+        private Stopwatch MyTimer = new Stopwatch();//确认累计计时器
+        private List<MessageClass> MessageList = new List<MessageClass>();//报文段信息列表
+        public server()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;//去掉控件的跨线程非法访问属性
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            readThread = new Thread(new ThreadStart(RunServer));
+            readThread.Start();
+
+            timelimitThread = new Thread(new ThreadStart(TimeScan));
+            timelimitThread.Start();
+
+            ShowThread = new Thread(new ThreadStart(SHOWDetail));
+            ShowThread.Start();
+        }
+        private void RunServer()
+        {
+            Int32 port;
+            int.TryParse(Port.Text, out port);
+            MyListener = new TcpListener(IPAddress.Parse(IPAddr.Text), port);
+            
+            //MyListener = new TcpListener(IPAddress.Parse("127.0.0.1"), 8088);
+            MyListener.Start();
+            Serstatus.Text = "服务启动";
+            socketatServer = MyListener.AcceptSocket();
+            ConnectOnline = true;
+            Serstatus.Text = "连接建立成功";
+
+            byte[] ReceiveMessage = new byte[500];
+            while (ConnectOnline)
+            {
+                if(socketatServer.Available>0)
+                {
+                    int num = socketatServer.Receive(ReceiveMessage);
+                    
+                    if (num > 0)
+                    {
+                        byte tempbyte = OutCheck(ReceiveMessage);
+                        if (tempbyte != ReceiveMessage[ReceiveMessage[2] - 1])
+                            MessageBox.Show("校验错误", "attention");
+                        Random random = new Random();//随机数种子
+                        int action = random.Next(0, 3);//随机选择本次接收的结果
+                        //int action = 0;
+                        byte[] tempMessage = new byte[ReceiveMessage[2]];
+                        Array.Copy(ReceiveMessage, tempMessage, ReceiveMessage[2]);
+                        tempMessage[0] = 32;//空格
+                        tempMessage[1] = 32;
+                        tempMessage[2] = 32;
+                        tempMessage[ReceiveMessage[2] - 1] = 32;//校验信息位
+                        if (action == 1)//丢失
+                        {
+                            string str = Encoding.Default.GetString(tempMessage);
+                            ShowDetail.AppendText("报文：" + str + "  " + ReceiveMessage[1]+"  丢失" + "\r\n");
+                            continue;
+                        }
+                        if (action == 2)//校验错误
+                        {
+                            string str = Encoding.Default.GetString(tempMessage);
+                            ShowDetail.AppendText("报文：" + str + "  " + ReceiveMessage[1] + "  校验错误" + "\r\n");
+                            continue;
+                        }
+                        if (ReceiveMessage[1] < P1 || ReceiveMessage[1] > P3)
+                        {
+                            continue;
+                        }
+                        bool IsRepeat = false;//重复报文标志
+                        foreach(var item in MessageList)
+                        {
+                            if (item.IdentiFication == ReceiveMessage[1])
+                            {
+                                IsRepeat = true;
+                                break;
+                            }//说明这是发送端由于长时间收不到确认消息而重发的，急需确认
+                        }
+                        if (IsRepeat == false)
+                        {
+                            MessageList.Add(new MessageClass { IdentiFication = ReceiveMessage[1], IsSendReceive=false,ReceiveTime= System.DateTime.Now, message = ReceiveMessage });
+                            MessageList = MessageList.OrderBy(MessageClass => MessageClass.IdentiFication).ToList();
+                            accumulator++;
+                            
+                            string str = Encoding.Default.GetString(tempMessage);
+                            ShowDetail.AppendText("收到报文：" + str + "  "+ReceiveMessage[1] + "\r\n");
+                            //ShowDetail.AppendText();
+                            if (accumulator != 3)
+                            {
+                                continue;
+                            }
+                        }
+                        else
+                        {
+                            string str = Encoding.Default.GetString(tempMessage);
+                            ShowDetail.AppendText("报文：" + str + "  " + ReceiveMessage[1] + "  重复" + "\r\n");
+                            continue;
+                        }
+                        int MaxIdentiFication = 0;
+                        foreach(var item in MessageList)//查找到最大的、允许返回确认消息的报文段标识
+                        {
+                            MaxIdentiFication++;
+                            item.IsSendReceive = true;//在之后，此报文段会被发送确认消息
+                            if(MaxIdentiFication != item.IdentiFication)//发现了标识不连续
+                            {
+                                item.IsSendReceive = false;
+                                MaxIdentiFication--;
+                                break;
+                            }
+                        }
+                        byte[] sendAck = new byte[5];
+                        sendAck[0] = 1;//报文类型为确认消息
+                        //sendAck[1] = 4;//测试用，ACK=4
+                        sendAck[1] = IntToBitConverter(MaxIdentiFication+1)[0];//报文标识
+                        int tempSize;
+                        int.TryParse(SendSize.Text, out tempSize);
+                        sendAck[2] = IntToBitConverter(tempSize)[0];//设置发送窗口大小
+                        socketatServer.Send(sendAck);//发送确认消息
+                        ShowDetail.AppendText("（累加器为3）发送ACK：" + sendAck[1] + "\r\n");
+                        P1 = MaxIdentiFication + 1;
+                        P3 = P1 + 4;
+                        accumulator = 0;
+                        MyTimer.Start();
+                    }                    
+                }
+                Thread.Sleep(1500);
+            }
+        }
+
+        private byte OutCheck(byte[] receiveMessage)
+        {
+            byte temp = 0;
+            for (int i = 0; i < receiveMessage[2]-1; i++)
+            {
+                temp += receiveMessage[i];
+            }
+            return temp;
+        }
+
+        public byte[] IntToBitConverter(int num)
+        {
+            byte[] bytes = BitConverter.GetBytes(num);
+            return bytes;
+        }
+        private void TimeScan()
+        {
+            while(true)
+            {
+                bool IsFind = false;
+                foreach(var item in MessageList)
+                {
+                    DateTime now = System.DateTime.Now;
+                    if (item.IsSendReceive==false&&((now.Minute-item.ReceiveTime.Minute>0)||(now.Second - item.ReceiveTime.Second > 9)))
+                    {
+                        item.ReceiveTime = now;//!!!!!!!!!!!!!!!!此处待考虑，这是为了避免短时间内重复发送
+                        IsFind = true;
+                        break;
+                    }
+                }
+                if (IsFind == true)
+                {
+                    int MaxIdentiFication = 0;
+                    foreach(var item in MessageList)//查找最大的、允许返回确认消息的报文标识
+                    {
+                        MaxIdentiFication++;
+                        item.IsSendReceive = true;//在之后，此报文段会被发送确认消息
+                        if (MaxIdentiFication != item.IdentiFication)
+                        {
+                            item.IsSendReceive = false;
+                            MaxIdentiFication--;
+                            break;
+                        }
+                    }
+                    byte[] sendAck = new byte[5];
+                    sendAck[0] = 1;//报文类型为确认消息                                
+                    sendAck[1] = IntToBitConverter(MaxIdentiFication + 1)[0];
+                    //sendAck[2] = 5;//设置发送窗口大小
+                    int tempSize;
+                    int.TryParse(SendSize.Text, out tempSize);
+                    sendAck[2] = IntToBitConverter(tempSize)[0];//设置发送窗口大小
+                    socketatServer.Send(sendAck);//发送确认消息
+                    ShowDetail.AppendText("(超过时限)发送ACK：" + sendAck[1] + "\r\n");
+
+                    P1 = MaxIdentiFication + 1;
+                    P3 = P1 + 4;
+                    accumulator = 0;
+                }
+                Thread.Sleep(500);
+            }
+            
+        }
+        private void SHOWDetail()
+        {
+            while (true)
+            {
+                WindLoca1.Text = "";
+                for (int i = P1; i <= P3; i++)
+                {
+                    WindLoca1.AppendText(i + " ");
+                }
+                Thread.Sleep(2000);
+            }
+
+        }
+        private void closeserver_Click(object sender, EventArgs e)
+        {
+            ConnectOnline = false;
+            socketatServer.Close();
+            MyListener.Stop();
+            Serstatus.Text = "服务关闭";
+            MessageBox.Show("服务器关闭！", "服务器");
+        }
+    }
+    public class MessageClass
+    {
+        public int IdentiFication;
+        public bool IsSendReceive;
+        public DateTime ReceiveTime;
+        public byte[] message = new byte[500];
+
+        public MessageClass()
+        {
+
+        }
+    }
+}
